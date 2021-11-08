c511b757bf8d8b99f7c9d9b26a5e07be7551a925
sandyjswl <sandeepjswl123@gmail.com>
2021-11-08 16:46:26 +0530

    Remove redis dependency// use dict as temp storage

--- app.py
+++ app.py
@@ -8,49 +8,44 @@ import ctypes
 app = Flask(__name__)
 
 
-candidates = redis.Redis(
-host=os.getenv("REDIS_HOST"),
-port=int(os.getenv("REDIS_PORT")),
-charset="utf-8", decode_responses=True)
+candidates = {}
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 
-@app.route('/check')
+@app.route('/health')
 def check():
-    return 'Checking In if everything is fine'
+    return 'Application is Up'
 
 
 @app.route('/vote/<candidate>')
 def vote(candidate):
-    current_candidate = candidates.get(candidate.upper())
-    if current_candidate is not None:
-        candidates.set(candidate.upper(), int(candidates.get(candidate.upper())) + 1 )
+    if candidate.upper() in candidates:
+        candidates[candidate.upper()] = candidates[candidate.upper()]  + 1
         return jsonify(success=True)
     return jsonify(success=False)
 
-@app.route('/register', methods = ['POST'])
+@app.route('/register')
 def register():
     try:
-        data =request.get_json()
-        candidates.set(data['name'].upper(),0)
+        candidates[request.args.get('name').upper()] = 0
         return jsonify(success=True)
     except:
         return jsonify(success=False)
 
 @app.route('/candidates', methods = ['GET'])
 def get_candidates():
-    keys = candidates.keys('*')
-    return jsonify(keys)
+    keys = candidates.keys()
+    return jsonify({"response":list(keys)})
 
 @app.route('/winner', methods = ['GET'])
 def get_winner():
     candidates_dict = {}
-    keys = candidates.keys('*')
+    keys = candidates.keys()
     for key in keys:
-        val = candidates.get(key)
+        val = candidates[key]
         candidates_dict[key] = int(val)
     try:
         print(candidates_dict)
@@ -63,12 +58,12 @@ def get_winner():
 def crash_app():
     os._exit(0)
 
-@app.route('/version', methods = ['GET'])
-def get_version():
-    version = os.getenv("VERSION")
-    if version is not None:
-        return jsonify(version)
-    return jsonify(success=False)
+# @app.route('/version', methods = ['GET'])
+# def get_version():
+#     version = os.getenv("VERSION")
+#     if version is not None:
+#         return jsonify(version)
+#     return jsonify(success=False)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, host='0.0.0.0')
